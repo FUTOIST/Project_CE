117057487831a32a2777d883097930b6f71ddbe2
FUTOIST <irfarnpohsu2545@gmail.com>
2025-03-12 09:06:49 +0700

    แต่ง Login เพิ่ม

--- index.php
+++ index.php
@@ -13,51 +13,50 @@ require_once 'db_pao.php';
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="css/index.css">
+
 </head>
 
 <body>
-    <div class="container">
-        <h3 class="mt-4">เข้าสู่ระบบ</h3>
+    <div class="login-box">
+        <h3 class="text-center text-success">เข้าสู่ระบบ</h3>
         <hr>
-        <form action="signin_pao.php" method="POST">
+        <div class="login-form">
+            <form action="signin_pao.php" method="POST">
+                <?php if (isset($_SESSION['success'])) { ?>
+                    <div class="alert alert-success">
+                        <?php echo $_SESSION['success'];
+                        unset($_SESSION['success']); ?>
+                    </div>
+                <?php } ?>
 
-            <?php if (isset($_SESSION['success'])) { ?>
-                <div class="alert alert-success">
-                    <?php
-                    echo $_SESSION['success'];
-                    unset($_SESSION['success']);
-                    ?>
-                    <script>
-                        setTimeout(function() {
-                            location.reload();
-                        }, 1500); // 
-                    </script>
+                <?php if (isset($_SESSION['error'])) { ?>
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <?php echo $_SESSION['error'];
+                        unset($_SESSION['error']); ?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                    </div>
+                <?php } ?>
+
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" name="email" required>
                 </div>
-            <?php } ?>
-
-            <?php if (isset($_SESSION['error'])) { ?>
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <?php
-                    echo $_SESSION['error'];
-                    unset($_SESSION['error']);
-                    ?>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" required>
                 </div>
-            <?php } ?>
-
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" name="email"
-                    aria-describedby="email">
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" name="password"
-                    aria-describedby="password">
-            </div>
-            <button type="submit" name="signin" class="btn btn-primary">Login</button>
-        </form>
+                <button type="submit" name="signin" class="btn btn-green w-100">Login</button>
+            </form>
+        </div>
     </div>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
